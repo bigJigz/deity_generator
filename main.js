e79021bb6f2a0edcd84bb8b81e840e80c55c6efa
bigJigz <59096606+bigJigz@users.noreply.github.com>
2022-08-20 16:17:37 -0400

    create deity factory

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('Generate Random Deity');
+
+//↓↓Factory Function Generates Deity↓↓\\
+generateDeity = (name, age, power, design) => {
+  return {
+    name: name,
+    age: age,
+    power: power,
+    design: design
+  }
+}
+
