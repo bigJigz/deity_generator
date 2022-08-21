02c6ca7263974932611e216afd26450ece38f697
BIGJIGZ <jean.nimer@rocketmail.com>
2022-08-21 16:20:35 -0700

    add/test name generator

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 console.log('Generate Random Deity');
 
 //↓↓Factory Function Generates Deity↓↓\\
-generateDeity = (name, age, power, design) => {
+generateDeity = (name, age, align, type) => {
   return {
     name: name,
     age: age,
-    power: power,
-    design: design
+    align: align,
+    type: type,
   }
 }
 
+nameGen = () => {
+  let fullName = "";
+  const nameStart = ['Var','Kreg', 'Drask', 'Bel', 'Ness', 'Hov', 'Olg', 'Zed', 'Demmu', 'Ammit', 'Ceph', 'Nef', 'Mal'];
+  const nameEnd = ['ander', 'andra', 'mana', 'drani', 'rappan', 'pathi', 'ravas', 'sin', 'grey', 'knod', 'binn', 'lor'];
+  fullName = nameStart[Math.floor(Math.random()*nameStart.length)] + nameEnd[Math.floor(Math.random()*nameEnd.length)];
+  return fullName;
+}
+
+
+
+console.log(generateDeity(nameGen(), 1, 'neutral', 'dragon'));
